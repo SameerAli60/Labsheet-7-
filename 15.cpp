726e69f58561e353be62b2400416f2e4564b0d33
SameerAli60 <cu25260290@coeruniversity.ac.in>
2025-11-06 22:05:18 +0530

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+ char a[100],b[50];
+ cin.getline(a,100); cin.getline(b,50);
+ cout<<(strstr(a,b)?"Present":"Not Present");
+}
