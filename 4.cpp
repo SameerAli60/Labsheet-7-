fb3c6548bb0045cd5d6c7b96e079805434a74a61
SameerAli60 <cu25260290@coeruniversity.ac.in>
2025-11-06 21:50:03 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+ char a[100],b[100];
+ cin.getline(a,100); cin.getline(b,100);
+ cout<<(strcmp(a,b)==0?"Equal":"Not Equal");
+}
